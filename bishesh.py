87b87184c0cc480d15f81bf8ff271e557af22aa5
bisheshxettri1 <154910016+bisheshxettri1@users.noreply.github.com>
2024-01-05 16:18:39 +0545

    Update bishesh.py

--- bishesh.py
+++ bishesh.py
@@ -202,9 +202,9 @@ import getpass
 attemps = 0
 
 while attemps < 12345677901:
-   username = input(' \033[0;92mEnter Username: ')
-   password = input(' \033[0;93mEnter Password : ')
-    if username == 'BISHESH' and password) == 'HONII':
+    username = input(' \033[0;92mEnter Username: ')
+    password = input(' \033[0;93mEnter Password: ')
+    if username == 'B' and password == '7':
         print(' \033[0;92mYou Have Successfully Logged in.')
         break
   
