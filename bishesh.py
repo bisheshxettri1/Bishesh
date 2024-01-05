21897f1d0de221500c6e6e4c628bf8316c44c588
bisheshxettri1 <154910016+bisheshxettri1@users.noreply.github.com>
2024-01-05 16:14:49 +0545

    Update bishesh.py

--- bishesh.py
+++ bishesh.py
@@ -202,9 +202,9 @@ import getpass
 attemps = 0
 
 while attemps < 12345677901:
-   (espeak-a) username = input(' \033[0;92mEnter Username: ')
-    (espeak-a) password = input(' \033[0;93mEnter Password: ')
-    if username == 'BISHESH' and password == 'HONII':
+   (espeak -a 200 username )= input(' \033[0;92mEnter Username: ')
+    (espeak -a 100 password = input(' \033[0;93mEnter Password: ')
+    if username == 'BISHESH' and password) == 'HONII':
         print(' \033[0;92mYou Have Successfully Logged in.')
         break
   
